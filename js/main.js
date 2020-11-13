33e2cf2f523d6284ffebd545b6a82b8f0ee142ad
Jenny <69027218+jennn613@users.noreply.github.com>
2020-11-13 18:04:28 +0900

    slider error

--- js/main.js
+++ js/main.js
@@ -138,136 +138,139 @@ for(let i=0; i<devicePicList.length; i++){
 let imgUrl; // s8 사진 받아오기
 
 //s8 c1 (black)
-function sDeviceImge(color) {
 
-    deviceSlider.innerHTML = " ";
-    let deviceColor;
-    switch(color){
-        case 0:
-            deviceColor = 'c1';
-            break;
-        case 1:
-            deviceColor = 'c2';
-            break;
-        case 2:
-            deviceColor = 'c3';
-            break;
-        default:
-            deviceColor = 'c1';
-    }
+// -----------------------------------
+// let devicePicListAdd, liContent, devicePicListAdded, imgIdx,newPicList, devicePicWidth;
+// // -----------------------------------
+// function sDeviceImge(color) {
+
+//     deviceSlider.innerHTML = " ";
+//     let deviceColor;
+//     switch(color){
+//         case 0:
+//             deviceColor = 'c1';
+//             break;
+//         case 1:
+//             deviceColor = 'c2';
+//             break;
+//         case 2:
+//             deviceColor = 'c3';
+//             break;
+//         default:
+//             deviceColor = 'c1';
+//     }
     
-    for(let i=0; i<devicePicNum; i++){
+//     for(let i=0; i<devicePicNum; i++){
     
-    let devicePicListAdd = document.createElement('li'); //li 추가
-    let liContent =  `<figure><img src=""><figcaption class="hidden"></figcaption></figure>`;
+//     devicePicListAdd = document.createElement('li'); //li 추가
+//     liContent =  `<figure><img src=""><figcaption class="hidden"></figcaption></figure>`;
 
-    imgUrl ='../css/img_galaxy/galaxys8/galaxy-s8_gallery-color_normal-'+deviceColor+'-0' +  (i+1) + '.jpg';
+//     imgUrl ='../css/img_galaxy/galaxys8/galaxy-s8_gallery-color_normal-'+deviceColor+'-0' +  (i+1) + '.jpg';
 
-    devicePicListAdd.innerHTML = liContent;
-    deviceSlider.append(devicePicListAdd);
+//     devicePicListAdd.innerHTML = liContent;
+//     deviceSlider.append(devicePicListAdd);
 
-    deviceImg = devicePicListAdd.querySelectorAll('img');
+//     deviceImg = devicePicListAdd.querySelectorAll('img');
 
-    deviceImg.forEach(function(pic){
-        pic.setAttribute('src', imgUrl);
+//     deviceImg.forEach(function(pic){
+//         pic.setAttribute('src', imgUrl);
+//     });
 
-    });
-}};
+//  devicePicListAdded = deviceSlider.querySelectorAll('li'); //li
+//  for(let i=0; i<picShow; i++){
+//   let copyLastBox = devicePicListAdded[devicePicListAdded.length-(i+1)].cloneNode(true);
+//   deviceSlider.prepend(copyLastBox);
+// } // 마지막 박스 복사 해주기
 
-sDeviceImge(); //default로 s c1이 보이게
-colorListItems.forEach(function(btn, i){
-    btn.addEventListener('click', (e)=>{
-        e.preventDefault();
-        sDeviceImge(i);
-    });
-});
+// devicePicListAdded.forEach(function(list){
+//   list.style.width = devicePicWrap.offsetWidth + 'px';
+//   });
 
-const devicePicListAdded = deviceSlider.querySelectorAll('li'); //li
+// imgIdx = 0 ;
 
-for(let i=0; i<picShow; i++){
-    let copyLastBox = devicePicListAdded[devicePicListAdded.length-(i+1)].cloneNode(true);
-    deviceSlider.prepend(copyLastBox);
-} // 마지막 박스 복사 해주기
+// newPicList = deviceSlider.querySelectorAll('li');
+// devicePicWidth = devicePicListAdded[0].offsetWidth; //사진 하나 가로
+// deviceSlider.style.width = devicePicWidth * newPicList.length + 'px';
 
-devicePicListAdded.forEach(function(list){
-list.style.width = devicePicWrap.offsetWidth + 'px';
-}); // li width 값을 div width로
+// deviceSlider.style.marginLeft = -devicePicWidth + 'px';
+// // indicator class 초기화
+// colorIndicator.forEach(function(indi){
+//   indi.classList.remove('active');
+// });
+// colorIndicator[imgIdx].classList.add('active');
 
-const newPicList = deviceSlider.querySelectorAll('li');
-let devicePicWidth = devicePicListAdded[0].offsetWidth; //사진 하나 가로
-deviceSlider.style.width = devicePicWidth * newPicList.length + 'px';
 
-deviceSlider.style.marginLeft = -devicePicWidth + 'px';
-// indicator class 초기화
-colorIndicator.forEach(function(indi){
-    indi.classList.remove('active');});
 
-let imgIdx = 0 ;
-colorIndicator[imgIdx].classList.add('active');
+//   colorListItems.forEach(function(btn, i){
+//     btn.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         sDeviceImge(i);
+//     });
+// });
+// colorChevron[0].addEventListener('click', (a) => {
+//   a.preventDefault();
 
-colorChevron[0].addEventListener('click', (a) => {
+//   imgIdx--;
+//   deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
 
-a.preventDefault();
+// if(imgIdx <= -1){
+//     deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
+//     imgIdx = devicePicListAdded.length - picShow;
+// }
 
-imgIdx--;
-deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
+// if(imgIdx >= 0){
+//     colorIndicator[imgIdx].classList.add('active'); //눌렀을 때 추가해주기
 
-if(imgIdx <= -1){
-    deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
-    imgIdx = devicePicListAdded.length - picShow;
-}
+//   }else{
+//     colorIndicator[devicePicListAdded.length - picShow].classList.add('active'); //눌렀을 때 추가해주기 
+//   }
 
-colorIndicator.forEach((indi) => {
-    indi.classList.remove('active'); //active class 제거해주기 = default
-});
+// });
 
-if(imgIdx >= 0){
-    colorIndicator[imgIdx].classList.add('active'); //눌렀을 때 추가해주기
+// imgIdx = 0;
+// colorChevron[1].addEventListener('click', (a) => {
 
-  }else{
-    colorIndicator[devicePicListAdded.length - picShow].classList.add('active'); //눌렀을 때 추가해주기 
-  }
+//   a.preventDefault();
+  
+//   imgIdx++;
+//   deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
+  
+//   if(imgIdx >= devicePicListAdded.length - picShow){
+//       deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
+//       imgIdx = -1;
+//   }
+  
+//   colorIndicator.forEach((indi) => {
+//       indi.classList.remove('active'); //active class 제거해주기 = default
+//   });
+  
+//   if(imgIdx >= 0){
+//       colorIndicator[imgIdx].classList.add('active'); //눌렀을 때 추가해주기
+  
+//     }else{
+//       colorIndicator[devicePicListAdded.length - picShow].classList.add('active'); //눌렀을 때 추가해주기 
+//     }
+  
+//   });
 
-});
 
-colorChevron[1].addEventListener('click', (a) => {
+// colorIndicator.forEach((indi, idx) => {
 
-a.preventDefault();
+//   indi.addEventListener('click', (e)=> {
+//       e.preventDefault();
 
-imgIdx++;
-deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
+//       deviceSlider.style.left = -devicePicWidth * idx + 'px';
 
-if(imgIdx >= devicePicListAdded.length - picShow){
-    deviceSlider.style.left = -devicePicWidth * imgIdx + 'px';
-    imgIdx = -1;
-}
+//       colorIndicator.forEach((i) => {
+//           i.classList.remove('active');
+//       });
+//       indi.classList.add('active');
+//   });
+//   });
 
-colorIndicator.forEach((indi) => {
-    indi.classList.remove('active'); //active class 제거해주기 = default
-});
+// }};
 
-if(imgIdx >= 0){
-    colorIndicator[imgIdx].classList.add('active'); //눌렀을 때 추가해주기
-
-  }else{
-    colorIndicator[devicePicListAdded.length - picShow].classList.add('active'); //눌렀을 때 추가해주기 
-  }
-
-});
-
-colorIndicator.forEach((indi, idx) => {
-
-    indi.addEventListener('click', (e)=> {
-        e.preventDefault();
-
-        deviceSlider.style.left = -devicePicWidth * idx + 'px';
-
-        colorIndicator.forEach((i) => {
-            i.classList.remove('active');
-        });
-        indi.classList.add('active');
-    });
-    });
+// sDeviceImge();
 
 const topBtn = document.querySelector('.topBtn'); //고정된 버튼
 const footerBtnParent = document.querySelector('.footer_btns'); //버튼 parent
